d79faf83c38a9ddd19a15237ff096b74d9a2cda2
Sergey Prokopchuk <svp@anahoret.com>
2017-12-04 10:21:04 +0200

    Add sass-loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
   inject: 'body'
 })
+
+const ExtractTextPluginConfig = new ExtractTextPlugin('style.css')
 
 module.exports = {
   entry: './client/index.js',
@@ -18,9 +21,10 @@ module.exports = {
   },
   module: {
     loaders: [
+      { test: /\.scss$/, use: ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'sass-loader']}) },
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig, ExtractTextPluginConfig]
 }
