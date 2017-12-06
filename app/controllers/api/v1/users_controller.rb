b43f4f4a7c44d4b219f8546cb7c44872c53a1cf1
Sergey Prokopchuk <svp@anahoret.com>
2017-12-06 12:37:12 +0200

    Move config for tests to rails_helper

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def index
     respond_with Users.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
       resources :users
     end
   end
-
 end
