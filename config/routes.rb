b1703cd442203cbc7a162028ba0b2ea0fec64f0e
Sergey Prokopchuk <svp@anahoret.com>
2017-12-07 09:53:28 +0200

    Add ApiConstraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    namespace :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       devise_for :users
       resources :users
     end
