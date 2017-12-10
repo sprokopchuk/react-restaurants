2ee6db64256ef399257f6c42332e43dbf0d4a662
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2017-12-10 18:53:00 +0200

    Cleanup from devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      devise_for :users
       resources :users
     end
   end
